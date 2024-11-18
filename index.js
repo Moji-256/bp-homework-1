0cacc75582207bc8d26872722b7374b4ef3fd64c
sawarot <gtmkf123@gmail.com>
2024-11-18 18:30:59 +0700

    update : forget to make in function;-;

--- index.js
+++ index.js
@@ -1,87 +1,115 @@
+//
+
 const A1 = [1,2,3] , A2 = [4,5,6];  ///input :3///
-const mergeAr = A1.concat(A2); 
-console.log(mergeAr);
+
+function mergeArray(ar1,ar2){
+    const mergeAr = ar1.concat(ar2); 
+    return mergeAr
+}
+
+console.log(mergeArray(A1,A2));
 
 //
 
 const Af2 = [10,20,30,40,50];   ///input :3///
-const MoreThirty = Af2.filter((value)=>value >= 30)
-console.log(MoreThirty);
+function Morenum(Arrf2){
+    const MoreThirty = Arrf2.filter((value)=>value >= 30)
+    return MoreThirty
+}
+
+console.log(Morenum(Af2));
 
 //
 
 
 const Af3 = [5,6,7,2,4];
 //console.log(Af3);
-let numin= 2 ;
-Af3.splice(numin,1)
-console.log(Af3);
+
+let numin= 2 ;  //input 
+
+function removeValue(ArrayF3,indexmove){
+    ArrayF3.splice(indexmove,1)
+    return Af3 ;
+}
+
+console.log(removeValue(Af3,numin));
 
 //
-
-let rom = {
-    "I":1,
-    "V":5,
-    "X":10,
-    "L":50,
-    "C":100,
-    "D":500,
-    "M":1000
-};
 let inputro = "XIV" ///input :3///
 
-const romin = inputro.toUpperCase();
-let resaultin = 0 , pre = 0;
+function romantoint (){
+            
+        let rom = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+        };
 
-for (let i = romin.length-1; i >= 0; i--) {
-    const char = rom[romin[i]];
-    //console.log(char)
-    if (char < pre) {
-        resaultin = resaultin - char 
-    }
-    else if (char >= pre)
-    {
-        resaultin = resaultin + char
-    }
-    else{}
+        const romin = inputro.toUpperCase();
+        let resaultin = 0 , pre = 0;
 
-    pre = char ;
-}
-console.log(resaultin);
+        for (let i = romin.length-1; i >= 0; i--) {
+        const char = rom[romin[i]];
+        //console.log(char)
+        if (char < pre) {
+            resaultin = resaultin - char 
+        }
+        else if (char >= pre)
+        {
+            resaultin = resaultin + char
+        }
+        else{}
+        pre = char ;
+        }
+        return resaultin
+    }
+
+console.log(romantoint(inputro));
 
 //
 
 let Af5=[1,2,1,4,5,2,3,4,1,4];
 
-let sortAf=Af5.toSorted();
-let prenum = 0;
-let rearr =[];
-for (let i = 0; i < sortAf.length; i++) {
-    currnum=sortAf[i];
+function duplisort (ArrayF5){
+    let sortAf=ArrayF5.toSorted();
+    let prenum = 0;
+    let rearr =[];
+    for (let i = 0; i < sortAf.length; i++) {
+        currnum=sortAf[i];
 
-    if (currnum != prenum) {
-        rearr.push(currnum);
+        if (currnum != prenum) {
+            rearr.push(currnum);
+        }
+        else{}
+
+        prenum=currnum;
     }
-    else{}
-
-    prenum=currnum;
+    return rearr;
 }
-console.log(rearr);
+console.log(duplisort(Af5));
 
 //
 
 let Af6=[1,2,3,4,5];
-let afrotate=[];
 
 let n = 3 ;  ///input :3//
 
-for (let i = 0; i < Af6.length; i++) {
+function rotatearr(arrf6,n){
+    let afrotate=[];
 
-    let j = (i + n) % Af6.length;  //กันลืม // for หาตำแหน่งที่ต้องถูกย้าย
-    // console.log(j);
-    // afrotate.push(Af6[i])
-    afrotate[j] = Af6[i];   //ตำแหน่งหลังย้าย = ตัวเดิมที่ตน.i
-    
+    for (let i = 0; i < arrf6.length; i++) {
+
+        let j = (i + n) % arrf6.length;  //กันลืม // for หาตำแหน่งที่ต้องถูกย้าย
+        // console.log(j);
+        // afrotate.push(arrf6[i])
+        afrotate[j] = arrf6[i];   //ตำแหน่งหลังย้าย = ตัวเดิมที่ตน.i
+        
+    }
+    return afrotate
 }
 
-console.log(afrotate);
+console.log(rotatearr(Af6,n));
